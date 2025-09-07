a7d882508d6db1c400af15fe4bf49492e77b299d
ishowrunes <ishworisgod@gmail.com>
2025-09-07 15:17:47 +0545

    Add basic text field

---
+++ lib/src/form/src/text_field.dart
@@ -0,0 +1,110 @@
+// Copyright (c) 2025 Order of Runes Authors. All rights reserved.
+
+import 'package:basic_components/src/form/src/helper/decor_mixin.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:morf/morf.dart';
+import 'package:utils/utils.dart';
+
+class BasicTextField extends StatefulWidget {
+  const BasicTextField({
+    super.key,
+    required this.controller,
+    this.decoration,
+    this.inputFormatters,
+    this.obscureText = false,
+    this.obscuringCharacter = '*',
+    this.inputType,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.autofillHints = const [],
+    this.onTap,
+    this.readOnly = false,
+    this.requestFocus = false,
+    this.enabled,
+    this.textInputAction,
+  });
+
+  final InputController controller;
+  final List<TextInputFormatter>? inputFormatters;
+  final InputDecoration? decoration;
+  final TextInputType? inputType;
+  final bool obscureText;
+  final String obscuringCharacter;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final List<String> autofillHints;
+  final VoidCallback? onTap;
+  final bool readOnly;
+  final bool requestFocus;
+  final bool? enabled;
+  final TextInputAction? textInputAction;
+
+  @override
+  State<BasicTextField> createState() => _BasicTextFieldState();
+}
+
+class _BasicTextFieldState extends State<BasicTextField> with DecorMixin{
+  late TextEditingController _textEditingController;
+  late FocusNode? node;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+    if (widget.controller.value.isNotNullAndNotEmpty) {
+      _textEditingController.text = widget.controller.value!;
+    }
+    if (widget.requestFocus) {
+      node = FocusNode();
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        node!.requestFocus();
+      });
+    } else {
+      node = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.controller,
+      builder: (context, _) {
+        if (widget.controller.value.isNotNull) {
+          _textEditingController.text = widget.controller.value!;
+        }
+        final defaultDecoration = getDecoration(context, widget.controller);
+        final mergedDecoration = widget.decoration.isNull ? defaultDecoration : mergeDecoration(defaultDecoration, widget.decoration!);
+
+        return TextField(
+          focusNode: node,
+          enabled: widget.enabled,
+          readOnly: widget.readOnly,
+          controller: _textEditingController,
+          onChanged: widget.controller.onChanged,
+          obscureText: widget.obscureText,
+          obscuringCharacter: widget.obscuringCharacter,
+          inputFormatters: widget.inputFormatters,
+          keyboardType: widget.inputType,
+          autofillHints: widget.autofillHints,
+          onTap: widget.onTap,
+          textInputAction: widget.textInputAction,
+          onSubmitted: widget.controller.onSubmitted,
+          decoration: resolveDecoration(
+            mergedDecoration,
+            prefixIcon: widget.prefixIcon,
+            suffixIcon: widget.suffixIcon,
+            errorText: widget.controller.error,
+          ),
+        );
+      },
+    );
+  }
+}
